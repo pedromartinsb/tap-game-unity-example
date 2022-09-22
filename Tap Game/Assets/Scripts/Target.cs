620ebb3aaa29eae2649648d9bf555a4915e2230c
pedrocampos <pedro.campos@startaideia.com.br>
2022-09-22 20:52:12 -0300

    feat: Programando os itens do jogo

--- Tap Game/Assets/Scripts/Target.cs
+++ Tap Game/Assets/Scripts/Target.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    // Velocidade dos objetos
+    private float minSpeed = 12f;
+    private float maxSpeed = 16f;
+    private float maxTorque = 10f;
+
+    // Local que o objeto será criado
+    private float xRange = 5f;
+    private float ySpawnPos = -6f;
+
+    // Corpo do objeto
+    private Rigidbody rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(RandomForce(), ForceMode.Impulse);
+        rigidbody.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private Vector3 RandomForce() 
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 }
